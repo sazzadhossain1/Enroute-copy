cefe464623d08460831ecdcfe6bde24af770c672
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-06 01:49:45 +0600

    change slider and get slider from daisy ui and put some text

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,41 +1,136 @@
 import React from "react";
+import "./Banner.css";
+import Header from "../Header/Header";
 
-// Import Swiper React components
-import { Swiper, SwiperSlide } from "swiper/react";
+// // Import Swiper React components
+// import { Swiper, SwiperSlide } from "swiper/react";
 
-// Import Swiper styles
-import "swiper/css";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
+// // Import Swiper styles
+// import "swiper/css";
+// import "swiper/css/pagination";
+// import "swiper/css/navigation";
 
-// import required modules
-import { Pagination, Navigation, HashNavigation } from "swiper/modules";
+// // import required modules
+// import { Pagination, Navigation, HashNavigation } from "swiper/modules";
 
 const Banner = () => {
   return (
-    <div>
-      <Swiper
-        spaceBetween={30}
-        hashNavigation={{
-          watchState: true,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Pagination, Navigation, HashNavigation]}
-        className="mySwiper"
-      >
-        <SwiperSlide data-hash="slide1">Slide 1</SwiperSlide>
-        <SwiperSlide data-hash="slide2">Slide 2</SwiperSlide>
-        <SwiperSlide data-hash="slide3">Slide 3</SwiperSlide>
-        <SwiperSlide data-hash="slide4">Slide 4</SwiperSlide>
-        <SwiperSlide data-hash="slide5">Slide 5</SwiperSlide>
-        <SwiperSlide data-hash="slide6">Slide 6</SwiperSlide>
-        <SwiperSlide data-hash="slide7">Slide 7</SwiperSlide>
-        <SwiperSlide data-hash="slide8">Slide 8</SwiperSlide>
-        <SwiperSlide data-hash="slide9">Slide 9</SwiperSlide>
-      </Swiper>
+    // <div>
+    //   <Swiper
+    //     spaceBetween={30}
+    //     hashNavigation={{
+    //       watchState: true,
+    //     }}
+    //     pagination={{
+    //       clickable: true,
+    //     }}
+    //     navigation={true}
+    //     modules={[Pagination, Navigation, HashNavigation]}
+    //     className="mySwiper"
+    //   >
+    //     <SwiperSlide data-hash="slide1">
+    //       <img
+    //         src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-recruitment.jpg"
+    //         alt=""
+    //       />
+    //     </SwiperSlide>
+    //     <SwiperSlide data-hash="slide2">
+    //       <img
+    //         src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-managed-service.jpg"
+    //         alt=""
+    //       />
+    //     </SwiperSlide>
+    //     <SwiperSlide data-hash="slide3">
+    //       <img
+    //         src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-executive-edu.jpg"
+    //         alt=""
+    //       />
+    //     </SwiperSlide>
+    //     <SwiperSlide data-hash="slide4">
+    //       <img
+    //         src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-consulting.jpg"
+    //         alt=""
+    //       />
+    //     </SwiperSlide>
+    //   </Swiper>
+    // </div>
+    <div className="carousel w-full">
+      <div id="slide1" className="carousel-item relative w-full">
+        <img
+          src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-recruitment.jpg"
+          className="w-full"
+          alt=""
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide4" className="btn btn-circle">
+            ❮
+          </a>
+          <div>
+            <div>
+              <div></div>
+              <p>Formulating Competitive HR Advantage</p>
+              <div></div>
+            </div>
+
+            <h3>
+              Specialized <span>Human Resource </span> solution
+            </h3>
+            <p>
+              We take a bold approach to formulate and implement HR strategies
+              for competitive advantage
+            </p>
+            <button>GET STARTED</button>
+          </div>
+          <a href="#slide2" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide2" className="carousel-item relative w-full">
+        <img
+          src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-managed-service.jpg"
+          className="w-full"
+          alt=""
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide1" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide3" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide3" className="carousel-item relative w-full">
+        <img
+          src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-executive-edu.jpg"
+          className="w-full"
+          alt=""
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide2" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide4" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
+      <div id="slide4" className="carousel-item relative w-full">
+        <img
+          src="https://enroute.com.bd/wp-content/uploads/2022/05/home-slide-consulting.jpg"
+          className="w-full"
+          alt=""
+        />
+        <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <a href="#slide3" className="btn btn-circle">
+            ❮
+          </a>
+          <a href="#slide1" className="btn btn-circle">
+            ❯
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
