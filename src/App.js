399eb2d9bb4f10ece3e6d2b898be586a91d4059c
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-05 17:15:58 +0600

    add navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
+import Main from "./LayOut/Main";
+import Home from "./components/Home/Home";
+import Services from "./components/Services/Services";
+import Industry from "./components/Industry/Industry";
+import ContactUs from "./components/ContactUs/ContactUs";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>,
+        },
+        {
+          path: "/home",
+          element: <Home></Home>,
+        },
+        {
+          path: "/service",
+          element: <Services></Services>,
+        },
+        {
+          path: "/industry",
+          element: <Industry></Industry>,
+        },
+        {
+          path: "/contactUs",
+          element: <ContactUs></ContactUs>,
+        },
+      ],
+    },
+  ]);
   return (
     <div className="App">
-      <h1 className="text-2xl">This is app</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Industry/Industry.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Industry.css";
+
+const Industry = () => {
+  return (
+    <div>
+      <h2>This is industry</h2>
+    </div>
+  );
+};
+
+export default Industry;
