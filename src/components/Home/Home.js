2129f5dafa4495e25b754ab5e1d22eed62b74333
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-09 17:22:45 +0600

    add information in api and map api in home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,6 +4,7 @@ import "./Home.css";
 import EnrouteInternational from "../EnrouteInternational/EnrouteInternational";
 import { useLoaderData } from "react-router-dom";
 import ManagedService from "../ManagedService/ManagedService";
+import Recruitment from "../Recruitment/Recruitment";
 
 const Home = () => {
   const getApis = useLoaderData();
@@ -15,6 +16,11 @@ const Home = () => {
       <div>
         {getApis.map((getApi) => (
           <ManagedService getApi={getApi} key={getApi._id}></ManagedService>
+        ))}
+      </div>
+      <div>
+        {getApis[0].secondPart.map((sp) => (
+          <Recruitment sp={sp} key={sp._id}></Recruitment>
         ))}
       </div>
     </div>
---
+++ src/components/Recruitment/Recruitment.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Recruitment.css";
+
+const Recruitment = () => {
+  return <div></div>;
+};
+
+export default Recruitment;
