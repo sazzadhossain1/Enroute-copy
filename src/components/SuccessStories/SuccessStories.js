8d335479ba957c3dbb23f102529c4bbb92b34ee3
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-10 17:06:48 +0600

    show api information in successStories's component

--- src/components/SuccessStories/SuccessStories.js
+++ src/components/SuccessStories/SuccessStories.js
@@ -1,8 +1,18 @@
 import React from "react";
 import "./SuccessStories.css";
 
-const SuccessStories = () => {
-  return <div className=""></div>;
+const SuccessStories = ({ singleCart }) => {
+  console.log(singleCart);
+  const { img, heading } = singleCart;
+
+  return (
+    <div className="">
+      <div>
+        <img src={img} alt="" />
+        <p>{heading}</p>
+      </div>
+    </div>
+  );
 };
 
 export default SuccessStories;
