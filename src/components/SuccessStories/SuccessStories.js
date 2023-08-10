b31673161f800019bbc950553df27e2237f8a7db
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-10 18:04:03 +0600

    success stories cart's style done

--- src/components/SuccessStories/SuccessStories.js
+++ src/components/SuccessStories/SuccessStories.js
@@ -7,7 +7,7 @@ const SuccessStories = ({ singleCart }) => {
   const { img, heading } = singleCart;
 
   return (
-    <div className="">
+    <div className="mb-28">
       <div className="successStories-child-div">
         <img className="img-successStories" src={img} alt="" />
         <div className="text-successStories">
