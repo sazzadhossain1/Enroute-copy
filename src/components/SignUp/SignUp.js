2fe1e59eb3d6d2afa89a20779237b725954bc277
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-14 02:54:21 +0600

    create signUp page with style and responsiveness

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,7 +2,32 @@ import React from "react";
 import "./SignUp.css";
 
 const SignUp = () => {
-  return <div></div>;
+  return (
+    <div className="sign-up-parent-div">
+      <div className="signup-form">
+        <h2>Sign Up</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id="firstName" name="firstName" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id="lastName" name="lastName" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" name="email" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
