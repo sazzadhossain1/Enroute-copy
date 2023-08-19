e81762811f9eae399a12d1017d7b2b2272726a0d
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-20 02:37:47 +0600

    load api's data by id and show data in manageServiceProduct component

--- src/components/ManagedServiceProduct/ManagedServiceProduct.js
+++ src/components/ManagedServiceProduct/ManagedServiceProduct.js
@@ -1,12 +1,36 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import "./ManagedServiceProduct.css";
 
 const ManagedServiceProduct = () => {
   const getManageServiceProductApi = useLoaderData();
-  console.log(getManageServiceProductApi);
+  const {
+    img,
+    pTitleById,
+    headingOne,
+    headingTwo,
+    headingThree,
+    pFirst,
+    pSecond,
+  } = getManageServiceProductApi;
+  console.log(img);
   return (
-    <div>
-      <h1>This is manage service product</h1>
+    <div className="manageServiceProduct-grid-div">
+      <div>
+        <img src={img} alt="" />
+      </div>
+      <div>
+        <div>
+          <div></div>
+          <p>{pTitleById}</p>
+        </div>
+        <h2>
+          <span>{headingOne}</span> <span>{headingTwo}</span>{" "}
+          <span>{headingThree}</span>
+        </h2>
+        <p>{pFirst}</p>
+        <p>{pSecond}</p>
+      </div>
     </div>
   );
 };
