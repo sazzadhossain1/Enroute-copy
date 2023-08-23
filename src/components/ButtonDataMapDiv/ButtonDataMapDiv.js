8a24ad4680d36312e905842441f52f1ff9bc5645
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-24 01:46:33 +0600

    use map method in recruitmentButtonDataById compoenet for load data from server site and show data in ButtonDataMapDiv component with some style

---
+++ src/components/ButtonDataMapDiv/ButtonDataMapDiv.js
@@ -0,0 +1,116 @@
+import React from "react";
+import "./ButtonDataMapDiv.css";
+
+const ButtonDataMapDiv = ({ bdtData }) => {
+  console.log(bdtData);
+  const {
+    imgById,
+    pHeading,
+    heading,
+    pFirst,
+    pointOne,
+    pointTwo,
+    pointThree,
+    pointFour,
+    pointFive,
+    pointSix,
+    pointSeven,
+    pointEight,
+    pointNine,
+    pointTen,
+    pointEleven,
+    pointTwelve,
+    pointThirteen,
+    pointFourteen,
+    pointFifteen,
+    logo,
+  } = bdtData;
+  return (
+    <div className="buttonDataMapDiv-grid-div">
+      <div>
+        <div className="pHeading-heading-pFirst-container">
+          <div className="manageServiceProduct-flex-div">
+            <div className="blank-manageServiceProduct-div"></div>
+            <p className="p-title">{pHeading}</p>
+          </div>
+          <h2 className="heading">{heading}</h2>
+          <p>{pFirst}</p>
+        </div>
+        <div className="ul-grid-div">
+          <ul>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointOne}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointTwo}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointThree}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointFour}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointFive}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointSix}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+
+              <p>{pointSeven}</p>
+            </li>
+          </ul>
+          <ul>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointEight}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointNine}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointTen}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointEleven}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointTwelve}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+              <p>{pointThirteen}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+
+              <p>{pointFourteen}</p>
+            </li>
+            <li className="li-flex">
+              <img className="tick-mark-logo" src={logo} alt="" />
+
+              <p>{pointFifteen}</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <img src={imgById} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default ButtonDataMapDiv;
--- src/components/RecruitmentButtonDataById/RecruitmentButtonDataById.js
+++ src/components/RecruitmentButtonDataById/RecruitmentButtonDataById.js
@@ -1,21 +1,63 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import ButtonDataMapDiv from "../ButtonDataMapDiv/ButtonDataMapDiv";
 
 const RecruitmentButtonDataById = () => {
   const getDataById = useLoaderData();
-  const { backgroundTitle, backgroundImg } = getDataById;
-  console.log(backgroundTitle);
+  const {
+    backgroundTitle,
+    backgroundImg,
+    pTitleById,
+    headingOne,
+    headingTwo,
+    headingThree,
+    pFirst,
+    pSecond,
+    pThird,
+    img,
+    buttonData,
+  } = getDataById;
+  // console.log(buttonData);
   return (
-    <div
-      style={{
-        backgroundImage: `url(${backgroundImg})`,
-        backgroundRepeat: "no-repeat",
-        backgroundPosition: "center",
-        backgroundSize: "cover",
-        height: "70vh",
-      }}
-    >
-      {backgroundTitle}
+    <div>
+      <div
+        style={{
+          backgroundImage: `url(${backgroundImg})`,
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+          height: "70vh",
+        }}
+      >
+        <h3 className="backgroundTitle">{backgroundTitle}</h3>
+      </div>
+      <div className="manageServiceProduct-grid-div">
+        <div>
+          <img className="img-manageServiceProduct" src={img} alt="" />
+        </div>
+        <div className="manageServiceProduct-text-div">
+          <div className="manageServiceProduct-flex-div">
+            <div className="blank-manageServiceProduct-div"></div>
+            <p className="p-title">{pTitleById}</p>
+          </div>
+          <h2 className="first-heading-manageServiceProduct">
+            <span>{headingOne}</span>
+            <span className="common-color-red">{headingTwo}</span>
+            <span>{headingThree}</span>
+          </h2>
+          <p className="pFirst">{pFirst}</p>
+          <p className="pSecond">{pSecond}</p>
+          <p className="pSecond">{pThird}</p>
+        </div>
+      </div>
+      <div>
+        {buttonData.map((bdtData) => (
+          <ButtonDataMapDiv
+            bdtData={bdtData}
+            key={bdtData.id}
+          ></ButtonDataMapDiv>
+        ))}
+      </div>
     </div>
   );
 };
