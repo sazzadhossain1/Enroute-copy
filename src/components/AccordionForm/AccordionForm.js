918abacc15a94713cff526c69267b461cd9f72e2
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-26 18:40:39 +0600

    accordionForm component style done without responsiveness

--- src/components/AccordionForm/AccordionForm.js
+++ src/components/AccordionForm/AccordionForm.js
@@ -3,130 +3,227 @@ import "./AccordionForm.css";
 
 const AccordionForm = () => {
   return (
-    <div className="accordionForm-grid-div">
-      <div className="accordionForm-children-div">
-        <details className="details">
-          <summary className="summary">1.Employer Branding</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
+    <div className="accordionForm-second-parent-div">
+      <div className="accordionForm-service-grid-div">
+        <div>
+          <div className="blank-flex-div">
+            <div className="accordionForm-blank-div"></div>
+            <p>Reroute To Success With</p>
           </div>
-        </details>
-        <details className="details">
-          <summary className="summary">2.HR Process Development</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">Focus Group Discussion</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">4.MTO Hiring Program</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">MTO Development Program</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">Salary Survey</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
+          <h1 className="accordionForm-heading">
+            <span>Our</span> <span className="color">Services</span>
+          </h1>
+        </div>
+        <div>
+          <p>
+            Enroute provides hr consulting services according to the business
+            needs and requirements. Our human resource consulting services under
+            different domains has uplifted the company’s reputation in the
+            industry as one of the finest hr compliance consultants in
+            Bangladesh.
+          </p>
+        </div>
       </div>
-      <div className="accordionForm-children-div">
-        <details className="details">
-          <summary className="summary">1.Employer Branding</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">2.HR Process Development</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">Focus Group Discussion</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">4.MTO Hiring Program</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">MTO Development Program</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
-        <details className="details">
-          <summary className="summary">Salary Survey</summary>
-          <div>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Omnis
-              dolores nulla molestiae inventore molestias voluptatum dolorum
-              excepturi commodi similique perferendis.
-            </p>
-          </div>
-        </details>
+      <div className="accordionForm-grid-div">
+        <div className="accordionForm-children-div">
+          <details className="details">
+            <summary className="summary">1. Employer Branding</summary>
+            <div className="p-div">
+              <p>
+                Employer branding is the process of promoting a company, or an
+                organization, as the employer of choice to a desired target
+                group, one which a company needs and wants to recruit and
+                retain. We are connected with all the career counseling
+                departments, student bodies of the reputed public and private
+                universities. We have lot of experiences in campus recruitments,
+                job fair etc.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">2. HR Process Development</summary>
+            <div className="p-div">
+              <p>
+                We are successfully managing the hr process for many
+                organizations. We are experienced in developing hr policy,
+                developing organogram for different organizations.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">3. Focus Group Discussion</summary>
+            <div className="p-div">
+              <p>
+                A focus group discussion (FGD) is a good way to gather together
+                people from similar backgrounds or experiences to discuss a
+                specific topic of interest of the employees of the organization.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">4.MTO Hiring Program</summary>
+            <div className="p-div">
+              <p>
+                Enroute is arranging the MTO hiring program since 2012. In the
+                program we do:
+              </p>
+              <ul>
+                <li className="li-flex">
+                  1. Screening and shortlisting of candidates who meet
+                  qualifying requirements of the respective/client company
+                </li>
+                <li>
+                  2. Candidates appear for a competitive written test as per the
+                  requirements of the respective/client company
+                </li>
+                <li>
+                  3. Face to face interview with the respective/client company
+                </li>
+                <li>4. Medical test by the respective/client company</li>
+                <li>
+                  5. Final shortlisting, selection and offer by the
+                  respective/client company
+                </li>
+              </ul>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">5. MTO Development Program</summary>
+            <div className="p-div">
+              <p>
+                Enroute thinks Management Trainee Officer Development Program is
+                the best way to prepare employees to become managers in the
+                future. It equips them with the required skills, judgment, and
+                know-how of being an effective and responsible manager. The
+                benefits of having management trainees in a company are obvious;
+                it provides a good succession planning and improves morale.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">6. Salary Survey</summary>
+            <div className="p-div">
+              <p>
+                Salary surveys are conducted with numerous employers to
+                determine pay levels for specific job categories and are
+                generally conducted either by region, sector or job
+                classification for the purposes of comparability. Many
+                organizations want to use salary surveys to set the compensation
+                levels for their organizations; and for that support enroute is
+                there.
+              </p>
+            </div>
+          </details>
+        </div>
+        <div className="accordionForm-children-div">
+          <details className="details">
+            <summary className="summary">
+              7. Recruitment Process Development
+            </summary>
+            <div className="p-div">
+              <p>
+                Good recruitment is vital for every organization, finding the
+                right people for the right roles at the right time. It ensures
+                that the workforce has the relevant skills and abilities for the
+                organization’s current and future needs. Effective recruitment
+                is not just about filling an immediate vacancy but about having
+                an impact on longer-term issues, such as future skills
+                development, organizational performance and employer brand.
+                enroute offers different organizations support regarding
+                Recruitment Process Development.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">
+              8. Mid-level manager competency verification
+            </summary>
+            <div className="p-div">
+              <p>
+                Competency is the capability to apply or use the set of related
+                knowledge, skills, and abilities required to successfully
+                perform ‘critical work functions’ or tasks in a defined work
+                setting. Mid-Level Manager Competency Verification supports
+                organizations to:
+              </p>
+              <ul>
+                <li>1. Meet internal and external compliance requirements</li>
+                <li>
+                  2. Have confidence in the skills of their workforce and ensure
+                  employees are capable of working safely in high risk areas
+                </li>
+                <li>
+                  3. Ensures workers skills are relevant and current to the
+                  workplace environment and machinery and tools being used
+                </li>
+              </ul>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">
+              9. Performance evaluation process development
+            </summary>
+            <div className="p-div">
+              <p>
+                The primary goals of a performance evaluation system are to
+                provide an equitable measurement of an employee’s contribution
+                to the workforce, produce accurate appraisal documentation to
+                protect both the employee and employer, and obtain a high level
+                of quality and quantity in the work produced. To create a
+                performance evaluation system we help to establish these five
+                steps in an organization:
+              </p>
+              <ul>
+                <li>1. Develop an evaluation form.</li>
+                <li>2. Identify performance measures.</li>
+                <li>3. Set guidelines for feedback.</li>
+                <li>4. Create disciplinary and termination procedures.</li>
+                <li>5. Set an evaluation schedule.</li>
+              </ul>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">
+              10. Job description development
+            </summary>
+            <div className="p-div">
+              <p>
+                Job descriptions are a communication tool to tell coworkers
+                where their job leaves off and the job of another employee
+                starts. We support different organizations to develop their JD
+                because a good job description performs a number of important
+                functions: It describes the skills and competencies that are
+                needed to perform the role; It defines where the job fits within
+                the overall company hierarchy; It is used as the basis for the
+                employment contract; and. It is a valuable performance
+                management tool.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">11. Job evaluation</summary>
+            <div className="p-div">
+              <p>
+                A job evaluation is a systematic way of determining the
+                value/worth of a job in relation to other jobs in an
+                organization. It tries to make a systematic comparison between
+                jobs to assess their relative worth for the purpose of
+                establishing a rational pay structure. enroute provides
+                trainings on Job Evaluation process.
+              </p>
+            </div>
+          </details>
+          <details className="details">
+            <summary className="summary">12. Outplacement service</summary>
+            <div className="p-div">
+              <p>
+                We provide the services to the employees of the different
+                organizations for outplacement. We facilitate during merger,
+                acquisition and company closer.
+              </p>
+            </div>
+          </details>
+        </div>
       </div>
     </div>
   );
