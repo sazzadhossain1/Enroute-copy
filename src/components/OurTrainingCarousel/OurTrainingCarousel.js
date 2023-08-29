f4aa39200683abf5767459d97dadc0f9bf4c6fe2
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-29 16:31:46 +0600

    add react multi carousel and add some text in carousel with a tick mark logo

--- src/components/OurTrainingCarousel/OurTrainingCarousel.js
+++ src/components/OurTrainingCarousel/OurTrainingCarousel.js
@@ -1,18 +1,38 @@
 import React from "react";
 import "./OurTrainingCarousel.css";
+import Carousel from "react-multi-carousel";
 
 const OurTrainingCarousel = () => {
+  const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5,
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+    },
+  };
   return (
-    <div>
-      <div>
+    <div className="ourTrainingCarousel-parent-div">
+      <div className="ourTrainingCarousel-grid-div">
         <div>
           <div className="Organizational-flex-div">
             <div className="Organizational-blank-div"></div>
             <p>Organizational Development Assurance With</p>
           </div>
-          <h1>
+          <h1 className="ourTrainingCarousel-heading">
             <span>
-              Enroute Signature <span>Courses</span>
+              Enroute Signature <span className="courses">Courses</span>
             </span>
           </h1>
         </div>
@@ -23,6 +43,412 @@ const OurTrainingCarousel = () => {
             tried and tested process ensures maximum output
           </p>
         </div>
+      </div>
+      <div className="carousel-div">
+        <Carousel responsive={responsive}>
+          <div>
+            <h3>Strategic Negotiation</h3>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Fundamentals of Negotiation</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Why negotiate?</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>3D Model</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Best Alternative to Negotiated Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Zone of Possible Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Playing with your tactics</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Handling Ethical issues</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Negotiation secrets</p>
+            </div>
+          </div>
+          <div>
+            <h3>Strategic Negotiation</h3>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Fundamentals of Negotiation</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Why negotiate?</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>3D Model</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Best Alternative to Negotiated Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Zone of Possible Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Playing with your tactics</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Handling Ethical issues</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Negotiation secrets</p>
+            </div>
+          </div>
+          <div>
+            <h3>Strategic Negotiation</h3>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Fundamentals of Negotiation</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Why negotiate?</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>3D Model</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Best Alternative to Negotiated Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Zone of Possible Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Playing with your tactics</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Handling Ethical issues</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Negotiation secrets</p>
+            </div>
+          </div>
+          <div>
+            <h3>Strategic Negotiation</h3>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Fundamentals of Negotiation</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Why negotiate?</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>3D Model</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Best Alternative to Negotiated Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Zone of Possible Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Playing with your tactics</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Handling Ethical issues</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Negotiation secrets</p>
+            </div>
+          </div>
+          <div>
+            <h3>Strategic Negotiation</h3>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Fundamentals of Negotiation</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Why negotiate?</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>3D Model</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Best Alternative to Negotiated Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Zone of Possible Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Playing with your tactics</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Handling Ethical issues</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Negotiation secrets</p>
+            </div>
+          </div>
+          <div>
+            <h3>Strategic Negotiation</h3>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Fundamentals of Negotiation</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Why negotiate?</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>3D Model</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Best Alternative to Negotiated Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Zone of Possible Agreement</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Playing with your tactics</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Handling Ethical issues</p>
+            </div>
+            <div>
+              <img
+                className="tick-mark-logo"
+                src="https://static.vecteezy.com/system/resources/previews/014/455/871/large_2x/green-check-mark-icon-on-transparent-background-free-png.png"
+                alt=""
+              />
+              <p>Negotiation secrets</p>
+            </div>
+          </div>
+        </Carousel>
       </div>
     </div>
   );
