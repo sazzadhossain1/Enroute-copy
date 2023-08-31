0332ffe41b9e715b7642d0493903d276d20e0061
sazzadhossain1 <sazzadtomal1@gmail.com>
2023-08-31 14:40:26 +0600

    add more logo and text with style

--- src/components/StrongFoothold/StrongFoothold.js
+++ src/components/StrongFoothold/StrongFoothold.js
@@ -15,35 +15,113 @@ const StrongFoothold = () => {
       <div className="strongFoothold-textAndLogo-grid-div">
         <div>
           <div className="strongFoothold-textAndLogo-div">
-            <div>
-              <h1>UN & INGO</h1>
-              <p>
+            <div className="strongFoothold-right-text-container">
+              <h1 className="strongFoothold-logo-heading">UN & INGO</h1>
+              <p className="strongFoothold-p">
                 Our collaboration with UN & INGO organizations resulted in
                 excellency in implementation & people-centric solutions that
                 served in solving the humanitarian crisis.
               </p>
             </div>
-            <img
-              className="strongFoothold-logo"
-              src="https://enroute.com.bd/wp-content/uploads/2022/05/UN.svg"
-              alt=""
-            />
+            <div className="strongFoothold-logo-div">
+              <img
+                className="strongFoothold-logo"
+                src="https://enroute.com.bd/wp-content/uploads/2022/05/UN.svg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="strongFoothold-textAndLogo-div">
+            <div className="strongFoothold-right-text-container">
+              <h1 className="strongFoothold-logo-heading">
+                FMCG – Fast Moving Consumer Goods
+              </h1>
+              <p className="strongFoothold-p">
+                We help FMCG companies prepare for the future by developing
+                consumer-centric innovation in dynamic market system.
+              </p>
+            </div>
+            <div className="strongFoothold-logo-div">
+              <img
+                className="strongFoothold-logo"
+                src="https://enroute.com.bd/wp-content/uploads/2022/05/FMCG.svg"
+                alt=""
+              />
+            </div>
+          </div>
+          <div className="strongFoothold-textAndLogo-div">
+            <div className="strongFoothold-right-text-container">
+              <h1 className="strongFoothold-logo-heading">
+                Education, Medical & Healthcare
+              </h1>
+              <p className="strongFoothold-p">
+                Our futuristic approach in the education, medical & healthcare
+                sectors is prioritized to accelerate social impact.
+              </p>
+            </div>
+            <div className="strongFoothold-logo-div">
+              <img
+                className="strongFoothold-logo"
+                src="https://enroute.com.bd/wp-content/uploads/2022/05/Education.svg"
+                alt=""
+              />
+            </div>
           </div>
         </div>
         {/* ------------------------------------------- */}
         <div>
           <div className="strongFoothold-textAndLogo-div">
-            <img
-              className="strongFoothold-logo"
-              src="https://enroute.com.bd/wp-content/uploads/2022/05/UN.svg"
-              alt=""
-            />
-            <div>
-              <h1>UN & INGO</h1>
-              <p>
-                Our collaboration with UN & INGO organizations resulted in
-                excellency in implementation & people-centric solutions that
-                served in solving the humanitarian crisis.
+            <div className="strongFoothold-logo-div">
+              <img
+                className="strongFoothold-logo"
+                src="https://enroute.com.bd/wp-content/uploads/2022/05/Logistics.svg"
+                alt=""
+              />
+            </div>
+            <div className="strongFoothold-left-text-container">
+              <h1 className="strongFoothold-logo-heading">Express Logistics</h1>
+              <p className="strongFoothold-p">
+                Enroute’s value-driven approach across the express logistics,
+                and transport industry positions companies for success in face
+                of current challenges.
+              </p>
+            </div>
+          </div>
+          <div className="strongFoothold-textAndLogo-div">
+            <div className="strongFoothold-logo-div">
+              <img
+                className="strongFoothold-logo"
+                src="https://enroute.com.bd/wp-content/uploads/2022/05/Telco.svg"
+                alt=""
+              />
+            </div>
+            <div className="strongFoothold-left-text-container">
+              <h1 className="strongFoothold-logo-heading">
+                Telco, Electronics & Network
+              </h1>
+              <p className="strongFoothold-p">
+                We have helped unleash the potential in Telco, Electronics &
+                Network Industries by developing capabilities and turning
+                possibilities into reality.
+              </p>
+            </div>
+          </div>
+          <div className="strongFoothold-textAndLogo-div">
+            <div className="strongFoothold-logo-div">
+              <img
+                className="strongFoothold-logo"
+                src="https://enroute.com.bd/wp-content/uploads/2022/05/Financial-Institutions.svg"
+                alt=""
+              />
+            </div>
+            <div className="strongFoothold-left-text-container">
+              <h1 className="strongFoothold-logo-heading">
+                Financial Institutions
+              </h1>
+              <p className="strongFoothold-p">
+                We are transforming the financial service through digitalization
+                and changes in fundamental aspects enabling their capabilities
+                for the future of banking and finance.
               </p>
             </div>
           </div>
